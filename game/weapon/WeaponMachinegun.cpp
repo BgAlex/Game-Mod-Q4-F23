3dd6451dabb5626693c19b8c613f939d1c83622a
BgAlex <bgalex2003@gmail.com>
2023-10-07 21:48:06 -0400

    Got the machine gun tomfoolery working.

--- game/weapon/WeaponMachinegun.cpp
+++ game/weapon/WeaponMachinegun.cpp
@@ -33,6 +33,19 @@ private:
 	stateResult_t		State_Reload		( const stateParms_t& parms );
 	stateResult_t		State_Flashlight	( const stateParms_t& parms );
 
+	int					chargeTime;
+	int					chargeDelay;
+	idVec2				chargeGlow;
+	bool				fireForced;
+	int					fireHeldTime;
+
+	bool				UpdateAttack(void);
+
+	stateResult_t		State_Charge(const stateParms_t& parms);
+	stateResult_t		State_Charged(const stateParms_t& parms);
+
+	bool				missile;
+
 	CLASS_STATES_PROTOTYPE ( rvWeaponMachinegun );
 };
 
@@ -58,6 +71,15 @@ void rvWeaponMachinegun::Spawn ( void ) {
 		
 	SetState ( "Raise", 0 );	
 	
+	chargeGlow = spawnArgs.GetVec2("chargeGlow");
+	chargeTime = SEC2MS(spawnArgs.GetFloat("chargeTime"));
+	chargeDelay = SEC2MS(spawnArgs.GetFloat("chargeDelay"));
+
+	fireHeldTime = 0;
+	fireForced = false;
+
+	missile = false;
+
 	Flashlight ( owner->IsFlashlightOn() );
 }
 
@@ -69,6 +91,11 @@ rvWeaponMachinegun::Save
 void rvWeaponMachinegun::Save ( idSaveGame *savefile ) const {
 	savefile->WriteFloat ( spreadZoom );
 	savefile->WriteBool ( fireHeld );
+	savefile->WriteInt(chargeTime);
+	savefile->WriteInt(chargeDelay);
+	savefile->WriteVec2(chargeGlow);
+	savefile->WriteBool(fireForced);
+	savefile->WriteInt(fireHeldTime);
 }
 
 /*
@@ -79,6 +106,11 @@ rvWeaponMachinegun::Restore
 void rvWeaponMachinegun::Restore ( idRestoreGame *savefile ) {
 	savefile->ReadFloat ( spreadZoom );
 	savefile->ReadBool ( fireHeld );
+	savefile->ReadInt(chargeTime);
+	savefile->ReadInt(chargeDelay);
+	savefile->ReadVec2(chargeGlow);
+	savefile->ReadBool(fireForced);
+	savefile->ReadInt(fireHeldTime);
 }
 
 /*
@@ -110,6 +142,61 @@ void rvWeaponMachinegun::Think()
 		zoomGui->SetStateFloat( "playerYaw", playerViewAxis.ToAngles().yaw );
 	}
 }
+
+bool rvWeaponMachinegun::UpdateAttack(void) {
+	// Clear fire forced
+
+	if (fireForced) {
+		if (!(wsfl.attack || wsfl.missile)) {
+			fireForced = false;
+		}
+		else {
+			return false;
+		}
+	}
+
+	// If the player is pressing the fire button and they have enough ammo for a shot
+	// then start the shooting process.
+	if ((wsfl.attack || wsfl.missile) && gameLocal.time >= nextAttackTime) {
+		missile = wsfl.missile;
+		// Save the time which the fire button was pressed
+		if (fireHeldTime == 0) {
+			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier(PMOD_FIRERATE));
+			fireHeldTime = gameLocal.time;
+			viewModel->SetShaderParm(6, chargeGlow[0]);
+		}
+	}
+
+	// If they have the charge mod and they have overcome the initial charge 
+	// delay then transition to the charge state.
+	if (fireHeldTime != 0) {
+		if (gameLocal.time - fireHeldTime > chargeDelay) {
+			SetState("Charge", 4);
+			return true;
+		}
+
+
+		// If the fire button was let go but was pressed at one point then 
+		// release the shot.
+		if (!(wsfl.attack || wsfl.missile)) {
+			idPlayer* player = gameLocal.GetLocalPlayer();
+			if (player) {
+
+				if (player->GuiActive()) {
+					//make sure the player isn't looking at a gui first
+					SetState("Lower", 0);
+				}
+				else {
+					SetState("Fire", 0);
+				}
+			}
+			return true;
+		}
+	}
+
+	return false;
+}
+
 
 /*
 ================
@@ -155,6 +242,8 @@ CLASS_STATES_DECLARATION ( rvWeaponMachinegun )
 	STATE ( "Fire",				rvWeaponMachinegun::State_Fire )
 	STATE ( "Reload",			rvWeaponMachinegun::State_Reload )
 	STATE ( "Flashlight",		rvWeaponMachinegun::State_Flashlight )
+	STATE ( "Charge",			rvWeaponMachinegun::State_Charge )
+	STATE ( "Charged",			rvWeaponMachinegun::State_Charged )
 END_CLASS_STATES
 
 /*
@@ -187,28 +276,20 @@ stateResult_t rvWeaponMachinegun::State_Idle( const stateParms_t& parms ) {
 				return SRESULT_DONE;
 			}
 
-			if ( fireHeld && !wsfl.attack ) {
-				fireHeld = false;
+			if (UpdateAttack()) {
+				return SRESULT_DONE;
 			}
-			if ( !clipSize ) {
-				if ( !fireHeld && gameLocal.time > nextAttackTime && wsfl.attack && AmmoAvailable ( ) ) {
-					SetState ( "Fire", 0 );
-					return SRESULT_DONE;
-				}
-			} else {
-				if ( !fireHeld && gameLocal.time > nextAttackTime && wsfl.attack && AmmoInClip ( ) ) {
-					SetState ( "Fire", 0 );
-					return SRESULT_DONE;
-				}  
-				if ( wsfl.attack && AutoReload() && !AmmoInClip ( ) && AmmoAvailable () ) {
-					SetState ( "Reload", 4 );
-					return SRESULT_DONE;			
-				}
-				if ( wsfl.netReload || (wsfl.reload && AmmoInClip() < ClipSize() && AmmoAvailable()>AmmoInClip()) ) {
-					SetState ( "Reload", 4 );
-					return SRESULT_DONE;			
-				}				
+
+			// Auto reload?
+			if (AutoReload() && !AmmoInClip() && AmmoAvailable()) {
+				SetState("reload", 2);
+				return SRESULT_DONE;
 			}
+			if (wsfl.netReload || (wsfl.reload && AmmoInClip() < ClipSize() && AmmoAvailable() > AmmoInClip())) {
+				SetState("Reload", 4);
+				return SRESULT_DONE;
+			}
+
 			return SRESULT_WAIT;
 	}
 	return SRESULT_ERROR;
@@ -226,6 +307,59 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 	};	
 	switch ( parms.stage ) {
 		case STAGE_INIT:
+			
+			idPlayer* player;
+			player = gameLocal.GetLocalPlayer();
+
+			if (player && player->GuiActive()) {
+				fireHeldTime = 0;
+				SetState("Lower", 0);
+				return SRESULT_DONE;
+			}
+
+			if (player && !player->CanFire()) {
+				fireHeldTime = 0;
+				SetState("Idle", 4);
+				return SRESULT_DONE;
+			}
+
+			if (missile)
+			{
+				if (gameLocal.time - fireHeldTime > chargeTime)
+				{
+					//3 means Super Missile
+					Attack(3, 1, 1, 0, 4.0f);
+					PlayAnim(ANIMCHANNEL_ALL, "fire", parms.blendFrames);
+					missile = false;
+				}
+				else
+				{
+					//2 means Regular Missile
+					Attack(2, 1, 1, 0, 1.0f);
+					PlayAnim(ANIMCHANNEL_ALL, "fire", parms.blendFrames);
+					missile = false;
+				}
+			}
+			else
+			{
+				if (gameLocal.time - fireHeldTime > chargeTime)
+				{
+					Attack(true, 1, 1, 0, 10.0f);
+					PlayEffect("fx_muzzleflash", barrelJointView, false);
+					PlayAnim(ANIMCHANNEL_ALL, "fire", parms.blendFrames);
+				}
+				else
+				{
+					//gameLocal.Printf("Regular Shot!\n");
+					Attack(false, 1, spread, 0, 1.0f);
+					PlayEffect("fx_muzzleflash", barrelJointView, false);
+					PlayAnim(ANIMCHANNEL_ALL, "fire", parms.blendFrames);
+				}
+			}
+
+			fireHeldTime = 0;
+
+			/*
 			if ( wsfl.zoom ) {
 				nextAttackTime = gameLocal.time + (altFireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
 				Attack ( true, 1, spreadZoom, 0, 1.0f );
@@ -234,10 +368,12 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 				nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
 				Attack ( false, 1, spread, 0, 1.0f );
 			}
-			PlayAnim ( ANIMCHANNEL_ALL, "fire", 0 );	
+			PlayAnim ( ANIMCHANNEL_ALL, "fire", 0 );
+			*/
 			return SRESULT_STAGE ( STAGE_WAIT );
 	
 		case STAGE_WAIT:		
+			/*
 			if ( !fireHeld && wsfl.attack && gameLocal.time >= nextAttackTime && AmmoInClip() && !wsfl.lowerWeapon ) {
 				SetState ( "Fire", 0 );
 				return SRESULT_DONE;
@@ -248,7 +384,15 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 			}		
 			if ( UpdateFlashlight ( ) ) {
 				return SRESULT_DONE;
-			}			
+			}
+			*/
+
+			if ((gameLocal.isMultiplayer && gameLocal.time >= nextAttackTime) ||
+				(!gameLocal.isMultiplayer && (AnimDone(ANIMCHANNEL_ALL, 60)))) {
+				SetState("Idle", 0);
+				return SRESULT_DONE;
+			}
+
 			return SRESULT_WAIT;
 	}
 	return SRESULT_ERROR;
@@ -322,6 +466,82 @@ stateResult_t rvWeaponMachinegun::State_Flashlight ( const stateParms_t& parms )
 			
 			SetState ( "Idle", 4 );
 			return SRESULT_DONE;
+	}
+	return SRESULT_ERROR;
+}
+
+/*
+================
+rvWeaponBlaster::State_Charge
+================
+*/
+stateResult_t rvWeaponMachinegun::State_Charge(const stateParms_t& parms) {
+	enum {
+		CHARGE_INIT,
+		CHARGE_WAIT,
+	};
+	bool muzzleTint = spawnArgs.GetBool("muzzleTint");
+	switch (parms.stage) {
+	case CHARGE_INIT:
+		viewModel->SetShaderParm(6, chargeGlow[0]);
+		StartSound("snd_charge", SND_CHANNEL_ITEM, 0, false, NULL);
+		//PlayCycle(ANIMCHANNEL_ALL, "fire", parms.blendFrames);
+		return SRESULT_STAGE(CHARGE_WAIT);
+
+	case CHARGE_WAIT:
+		if (gameLocal.time - fireHeldTime < chargeTime) {
+			float f;
+			f = (float)(gameLocal.time - fireHeldTime) / (float)chargeTime;
+			f = chargeGlow[0] + f * (chargeGlow[1] - chargeGlow[0]);
+			f = idMath::ClampFloat(chargeGlow[0], chargeGlow[1], f);
+			viewModel->SetShaderParm(6, f);
+			//ME ATTEMPING TO UHHHHH DO A SOMTHING.
+			viewModel->PlayEffect("fx_muzzleflash", flashJointView, false, vec3_origin, false, EC_IGNORE, muzzleTint ? owner->GetHitscanTint() : vec4_one);
+
+			if (!(wsfl.attack || wsfl.missile)) {
+				SetState("Fire", 0);
+				return SRESULT_DONE;
+			}
+
+			return SRESULT_WAIT;
+		}
+		SetState("Charged", 4);
+		return SRESULT_DONE;
+	}
+	return SRESULT_ERROR;
+}
+
+/*
+================
+rvWeaponBlaster::State_Charged
+================
+*/
+stateResult_t rvWeaponMachinegun::State_Charged(const stateParms_t& parms) {
+	enum {
+		CHARGED_INIT,
+		CHARGED_WAIT,
+	};
+
+	bool muzzleTint = spawnArgs.GetBool("muzzleTint");
+
+	switch (parms.stage) {
+	case CHARGED_INIT:
+		viewModel->SetShaderParm(6, 1.0f);
+
+		StopSound(SND_CHANNEL_ITEM, false);
+		StartSound("snd_charge_loop", SND_CHANNEL_ITEM, 0, false, NULL);
+		StartSound("snd_charge_click", SND_CHANNEL_BODY, 0, false, NULL);
+		return SRESULT_STAGE(CHARGED_WAIT);
+
+	case CHARGED_WAIT:
+		//ME ATTEMPING TO UHHHHH DO A SOMTHING.
+		viewModel->PlayEffect("fx_muzzleflashCharged", flashJointView, false, vec3_origin, false, EC_IGNORE, muzzleTint ? owner->GetHitscanTint() : vec4_one);
+		if (!(wsfl.attack || wsfl.missile)) {
+			fireForced = true;
+			SetState("Fire", 0);
+			return SRESULT_DONE;
+		}
+		return SRESULT_WAIT;
 	}
 	return SRESULT_ERROR;
 }
